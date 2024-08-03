522afbe126b543bcf3fe6efea3ad1d15c38c1a1a
Mayank-p19 <mayankpunia74@gmail.com>
2024-08-03 11:10:27 +0530

    login

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(bodyParser.json());
 
 const users = [];       //This is a placeholder. In a real project we'd use database.
-const secretKey = 
+const SECRET_KEY = 'your_secret_key';
 
 app.post('/register', async(req, res) => {
    const {username, password} = req.body;
@@ -21,6 +21,28 @@ app.post('/register', async(req, res) => {
 
    res.status(201).send('User registered');
 });
+
+
+app.post('/login', async (req, res) => {
+    const {username, password} = req.body;
+    const user = users.find(u => u.username === username);
+    if(!user){
+        return res.status(400).send('User not found');
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if(!isMatch){
+        return res.status(400).send('Invalid credentials');
+    }
+
+    const token = jwt.sign({username: user.username}, SECRET_KEY, {expiresIn: '1h'});
+    res.json({token});
+});
+
+const verifyToken = (req, res, next) => {
+    const token = req.header('Authorization');
+}
 
 app.listen(3000, () => {
     console.log('Server is running on the port 3000');
